7afa81cc1fb67c92e473d6c39b931496e9f561fc
Nehal Mahmud <mahmudnehal83@gmail.com>
2025-02-22 12:26:35 +0600

    ternery learned

---
+++ ternery.js
@@ -0,0 +1,9 @@
+// age = 9;
+
+// age >=18? console.log('you can vote'): console.log('you are not eligible');
+
+price = 600;
+isLeader = true;
+// price = isLeader=== true? console.log('0'): console.log(price+100);
+
+price = isLeader === true ? price > 1000 ? price/2: 0: price+100;
