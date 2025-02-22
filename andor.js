9876105d344e1bea8c866c1b5ee2c29807650602
Nehal Mahmud <mahmudnehal83@gmail.com>
2025-02-22 11:32:58 +0600

    first commit

---
+++ andor.js
@@ -0,0 +1,10 @@
+var salary = 20000;
+var isBCS = true;
+var height = 63;
+
+if(salary>15000 && isBCS==true || height>50){
+    console.log('Hea raji');
+}
+else{
+    console.log('na raji na');
+}
---
+++ if.js
@@ -0,0 +1,12 @@
+if(13<10){
+    console.log('i have a smaller pen');
+}
+
+var weight = 40;
+
+if(weight <20){
+    console.log('i will bring it')
+}
+else{
+    console.log('he will bring it')
+}
