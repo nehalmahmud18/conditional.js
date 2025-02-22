da29ced927847b86b553e6ca57e52d3cc2c56cc4
Nehal Mahmud <mahmudnehal83@gmail.com>
2025-02-22 12:02:39 +0600

    nested learned

---
+++ nested.js
@@ -0,0 +1,19 @@
+const money = -50;
+
+if(money >= 200){
+    console.log('borolox');
+
+}
+else{
+    if(money> 100){
+        console.log('gorib o na borolok o na');
+    }
+    else{
+        if(money> 0){
+            console.log('kola kha')
+        }
+        else{
+            console.log('goribs')
+        }
+    }
+}
