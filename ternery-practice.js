9e31bfd208679e65d041fddbc8b2513fab591f14
Nehal Mahmud <mahmudnehal83@gmail.com>
2025-02-22 13:04:58 +0600

    ternery practice done

---
+++ ternery-practice.js
@@ -0,0 +1,15 @@
+const num1= 5;
+const num2= 2;
+
+let result = 0;
+
+// if(num1 > num2){
+//     console.log(result = num1* 2)
+// }
+// else{
+//     console.log(result= num1 + num2)
+// }
+
+// TERNERY APPROACH
+result = (num1 > num2) ? num1 * 2 : num1 + num2;
+console.log(result);
